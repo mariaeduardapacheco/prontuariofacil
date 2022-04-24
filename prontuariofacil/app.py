ad543a3f62718fd96fba2d8667c5faba01b6f96f
Alonso <joaoalonso42@gmail.com>
2022-04-24 20:38:02 -0300

    teste ola

--- prontuariofacil/app.py
+++ prontuariofacil/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from ext import view
+from prontuariofacil.ext import view
 from prontuariofacil.ext import database
 from prontuariofacil.ext import config
 from prontuariofacil.ext import cli
@@ -11,5 +11,8 @@ def create_app():
     database.init_app(app)
     cli.init_app(app)
     view.init_app(app)
+    @app.route('/')
+    def comeco():
+        return "comeco"
     return app
     
