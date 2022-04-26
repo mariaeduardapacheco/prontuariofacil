1f68d0447cf90b7d0d400f3cb55a37f973cc4ab9
Alonso <joaoalonso42@gmail.com>
2022-04-25 22:14:05 -0300

    start forms

--- prontuariofacil/ext/view/main.py
+++ prontuariofacil/ext/view/main.py
@@ -1,4 +1,6 @@
 from flask  import Blueprint, render_template
+from ..models.models import Medico
+
 
 bp = Blueprint("view", __name__)
 
@@ -6,6 +8,12 @@ bp = Blueprint("view", __name__)
 def index():
     return render_template('index.html')
 
-@bp.route("/ola")
-def ola():
-    return "Olá"
+@bp.route("/formulario")
+def forms():
+    return render_template('forms.html')
+
+@bp.route("/user/add", methods=['GET','POST'])
+def add_user():
+    user = Medico()
+    
+    return render_template('user_add.html',form=form)
