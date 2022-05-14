e99cad9b0a637d3bb64d2e304ef99b995d249daf
Alonso <joaoalonso42@gmail.com>
2022-05-14 17:08:55 -0300

    crude adicionado

--- prontuariofacil/ext/view/main.py
+++ prontuariofacil/ext/view/main.py
@@ -1,7 +1,4 @@
-import email
-from mailbox import NotEmptyError
-from typing_extensions import Self
-from flask  import Blueprint, redirect, render_template, request
+from flask  import Blueprint, render_template, request
 from ext.models.tables import Medico, Prontuario
 from ext.models.form import Form, Form_medico
 from ext.database import db
@@ -17,55 +14,31 @@ def index():
 def forms():
     return render_template('forms.html')
 
-#@bp.route("/user/add", methods=['GET','POST'])
-# def add_user():
-#    user = Medico()    
-#    return render_template('user_add.html',user=user)
 
 @bp.route("/cadastro-prontuario", methods=["GET", "POST"])
 def cadastro_pront():
+    name = None
     form = Form()
-    nome = (form.nome.data)
-    cpf = (form.cpf.data)
-    nasc = (form.nasc.data)
-    sus = (form.sus.data)
-    alergia = (form.alergia.data)
-    doenca = (form.doenca.data)
-    historico = (form.historico.data)
-    
-    i = Prontuario()
-    i.alergia = alergia
-    i.nome = nome
-    i.cpf = cpf
-    i.nascimento = nasc
-    i.numsus = sus
-    i.doencas = doenca
-    i.historico_fami = historico
-    db.session.add(i)
-    db.session.commit()
-    
-    print(i.query.all())
 
-    '''
     if form.validate_on_submit:
-        prontuario = Prontuario(nome=form.nome, cpf=form.cpf, nascimento=form.nasc, numsus=form.sus, alergia=form.alergia, doencas=form.doenca ,historico_fami=form.historico)
+        prontuario = Prontuario(nome=form.nome.data, cpf=form.cpf.data, nascimento=form.nasc.data, numsus=form.sus.data, alergia=form.alergia.data, doencas=form.doenca.data ,historico_fami=form.historico.data)
         db.session.add(prontuario)
         db.session.commit()
-    '''
-    return render_template("cadastroPront.html", prontuario=form)
+    
+    return render_template("cadastroPront.html", prontuario=form, name = name)
 
-@bp.route("/cadastro-medico")
+@bp.route("/cadastro-medico", methods=["GET", "POST"])
 def cadastro_medico():
     form = Form_medico()
-    medico = Medico()
-    medico.nome = form.nome.data
-    medico.crm = form.crm.data
-    medico.email = form.email.data
-    medico.senha = form.cadastrar_senha.data
+    if form.validate_on_submit:
+        medico = Medico()
+        medico.nome = form.nome.data
+        medico.crm = form.crm.data
+        medico.email = form.email.data
+        medico.senha = form.cadastrar_senha.data
 
-    db.session.add(medico)
-    db.session.commit()
-
+        db.session.add(medico)
+        db.session.commit()
     print(medico.query.all())
 
     return render_template("cadastroMedico.html", form=form)
@@ -92,3 +65,43 @@ def escolha():
 @bp.route("/receiver", methods=["POST"])
 def receiver():
     return f"teste {request.values['nome']}"
+
+@bp.route("/update/<int:id>", methods=['GET','POST'])
+def update(id):
+    form = Form()
+    prontuario_update = Prontuario.query.get_or_404(id)
+    if request.method == "POST":
+            prontuario_update.nome = form.nome.data
+            prontuario_update.cpf = form.cpf.data
+            prontuario_update.nascimento = form.nasc.data
+            prontuario_update.numsus = form.sus.data
+            prontuario_update.alergia = form.alergia.data
+            prontuario_update.doencas = form.doenca.data
+            prontuario_update.historico_fami = form.historico.data
+            try:
+                db.session.commit()
+                print('update sucesso')
+                return render_template("update_prontuario.html", prontuario_update = prontuario_update, prontuario = form)
+            except:
+                print("ERRO no update!")
+                return render_template("update_prontuario.html", prontuario_update = prontuario_update, prontuario = form)
+    else:
+        return render_template("update_prontuario.html", prontuario_update = prontuario_update, prontuario = form)
+    
+
+
+
+@bp.route("/delete/<int:id>")
+def delete(id):
+    prontuari_delete =  Prontuario.query.get_or_404(id)
+    try:
+        db.session.delete(prontuari_delete)
+        db.session.commit()
+        print("Prontuario deletado com sucesso")
+        read = Prontuario.query.all()
+        return render_template("consultaProntuario.html", read=read)
+    except:
+        print("erro no delete")
+        read = Prontuario.query.all()
+        return render_template("consultaProntuario.html", read=read)
+    
